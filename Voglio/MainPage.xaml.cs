9d1bd5ed04862bfaeb6ab15f2a90562ae977b26f
bandblad <svegliato@MacBook-Air-Vladislav.local>
2018-09-15 01:25:37 +0400

    Hello, World

---
+++ Voglio/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Voglio
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var tapEstablishment = new TapGestureRecognizer()
+            {
+                Command = new Command(() => {
+                    establishmentPicker.Focus();
+                })
+            };
+
+            var tapCuisine = new TapGestureRecognizer()
+            {
+                Command = new Command(() => {
+                    cuisinePicker.Focus();
+                })
+            };
+
+            var tapTime = new TapGestureRecognizer()
+            {
+                Command = new Command(() => {
+                    timePicker.Focus();
+                })
+            };
+
+            establishmentLabel.GestureRecognizers.Add(tapEstablishment);
+            cuisineLabel.GestureRecognizers.Add(tapCuisine);
+            timeLabel.GestureRecognizers.Add(tapTime);
+        }
+    }
+}
