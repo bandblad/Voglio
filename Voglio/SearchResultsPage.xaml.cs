bf13e501af74be86d58c5adedc326a9c106bb657
bandblad <svegliato@MacBook-Air-Vladislav.local>
2018-09-15 23:43:24 +0400

    Created 'Search Results' page.

---
+++ Voglio/SearchResultsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Voglio
+{
+    public partial class SearchResultsPage : ContentPage
+    {
+        public SearchResultsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
